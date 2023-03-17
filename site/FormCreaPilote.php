8b9254aef89509b3c9f40fbbd6d118bc3526bc18
Mathéo Vignaud <64284499+Pokecreatorfr@users.noreply.github.com>
2023-03-17 13:39:55 +0100

    structure du projet

---
+++ site/FormCreaPilote.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Création Pilote</title>
+    <link rel="stylesheet" href="./assets/vendors/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="FormsCSS.css">
+</head>
+<body>  
+        <h1> Créer un pilote </h1>
+        <div class="mb-3">
+            <label for="FormInput" class="form-label">Nom</label>
+            <input type="Nom" class="form-control" id="exampleFormControlInput1" placeholder="">
+        </div>
+        <div class="mb-3">
+            <label for="FormInput" class="form-label">Prenom</label>
+            <input type="Prenom" class="form-control" id="exampleFormControlInput1" placeholder="">
+        </div>
+        <div class="mb-3">
+            <label for="FormInput" class="form-label">Centre</label>
+            <input type="Centre" class="form-control" id="exampleFormControlInput1" placeholder="">
+        </div>
+        <div class="mb-3">
+            <label for="FormInput" class="form-label" id="promo">Promotions Enseignées</label>
+            <input type="Promotions" class="form-control" id="exampleFormControlInput1" placeholder="">
+        </div>
+        <button type="button" class="btn btn-primary">Soumettre</button>
+    
+    <script src="./assets/vendors/jquery/jquery-3.6.0.min.js"></script>
+    <script src="./assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>    
+</body>
+</html>
---
+++ site/FormCreaPoste.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CréationEntreprise</title>
+    <link rel="stylesheet" href="./assets/vendors/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="./assets/vendors/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="FormsCSS.css">
+</head>
+<body>  
+        <h1 class="Poste">Intitulé du poste</h1>
+
+        <div class="mb-3">
+            <input type="Titre" class="form-control" id="exampleFormControlInput1" placeholder="Nom...">
+        </div>
+
+        <div class="mb-3">
+            <input type="Promotion" class="form-control" id="exampleFormControlInput1" placeholder="Prénom...">
+        </div>
+
+        <div class="mb-3">
+            <input type="Durée de Stage" class="form-control" id="exampleFormControlInput1" placeholder="Parlez nous de vous...">
+        </div>
+        <div class="row">
+
+            <div class="col fichiers">
+                <div class="input-group mb-3">
+                    <input type="file" class="form-control" id="inputGroupFile02">
+                    <label class="input-group-text" for="inputGroupFile02">Charger</label>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="input-group mb-3">
+                    <input type="file" class="form-control" id="inputGroupFile02">
+                    <label class="input-group-text" for="inputGroupFile02">Charger</label>
+                </div>
+            </div>
+        </div>
+
+        <button type="button" class="btn btn-primary">Soumettre</button>
+    
+    <script src="./assets/vendors/jquery/jquery-3.6.0.min.js"></script>
+    <script src="./assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>    
+</body>
+</html>
