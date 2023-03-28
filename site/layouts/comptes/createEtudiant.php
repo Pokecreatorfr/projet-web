f47e9be705a48e7171e3966bd3df5a1ef049b2b6
Mathéo Vignaud <64284499+Pokecreatorfr@users.noreply.github.com>
2023-03-28 21:12:31 +0200

    update

--- site/layouts/comptes/createEtudiant.php
+++ site/layouts/comptes/createEtudiant.php
@@ -23,6 +23,13 @@ require_once "../../config.php";
 </head>
 
 <body>
+      
+      <style>
+            .profile_img{
+    margin-left: 35%;
+    width: 40%;
+}
+      </style>
       <div class="container mt-5">
             <div class="card">
                   <h1 class="Offre card-header"> Créer un étudiant</h1>
@@ -64,10 +71,8 @@ require_once "../../config.php";
                                     <div class="col-md-6">
                                           <label for="FormInput" class="form-label Offre">Photo de profil
                                           </label>
-                                          <img src="\upload\profile_pics\default.png" class="profile_img"
-                                                id="profile_img" alt=""> </img>
-                                          <input class="form-control form-control-sm inpt" id="image_file" type="file"
-                                                accept="image/*" name="profile_img" />
+                                          <img src="\upload\profile_pics\default.png" class="profile_img" id="profile_img" alt="Responsive image" > </img>
+                                          <input class="form-control form-control-sm inpt" id="image_file" type="file" accept="image/*" name="profile_img"/> 
                                     </div>
 
                                     <div class="col-md-6">
@@ -132,15 +137,14 @@ require_once "../../config.php";
                                           <label class="form-label">Site</label>
                                           <div class="row">
                                                 <div class="mb-3">
-                                                      <select name="ville" id="SelectVille" class="form-select"
-                                                            aria-label="Default select example">
+                                                <input class="form-control" list="datalistOptions" id="exampleDataList" name="ville" placeholder="Commencez à ecrire...">
+                                                <datalist id="datalistOptions">
                                                             <?php
-                                                                        while ($tab = $villeSel->fetch()) {
-                                                                              echo '<option selected>Ville</option>';
-                                                                              echo '<option value="' . $tab[0] . '">' . $tab[1] . ' ' . $tab[2] . '</option>';
-                                                                        }
-                                                                        ?>
-                                                      </select>
+                                                                  while ($tab = $villeSel->fetch()) {
+                                                                        echo '<option value="' . $tab[1] . '(' . $tab[2]. ')' . '">'  . '</option>';
+                                                                  }
+                                                            ?>
+                                                </datalist>
                                                 </div>
                                           </div>
                                     </div>
@@ -161,31 +165,7 @@ require_once "../../config.php";
             integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
       </script>
 
-      <script>
-      img_file = document.getElementById("image_file");
-      profile_img = document.getElementById("profile_img");
 
-      img_file.addEventListener("change", function() {
-            let file = this.files[0];
-            let reader = new FileReader();
-            reader.readAsDataURL(file);
-
-            reader.onload = function(event) {
-                  let img = new Image();
-                  img.src = event.target.result;
-                  img.onload = function() {
-                        let canvas = document.createElement('canvas');
-                        let size = Math.min(img.width, img.height);
-                        canvas.width = size;
-                        canvas.height = size;
-                        let ctx = canvas.getContext('2d');
-                        ctx.drawImage(img, (img.width - size) / 2, (img.height - size) / 2,
-                              size, size, 0, 0, size, size);
-                        profile_img.src = canvas.toDataURL('image/jpeg');
-                  }
-            }
-      });
-      </script>
 
 </body>
 
