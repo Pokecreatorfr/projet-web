ef12f8b2287f08769d6e5c09cef1b6cb4f4e1f9a
Mathéo Vignaud <64284499+Pokecreatorfr@users.noreply.github.com>
2023-03-29 04:33:38 +0200

    Update wishlist.php

--- site/layouts/offres/wishlist.php
+++ site/layouts/offres/wishlist.php
@@ -11,11 +11,20 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 require_once "../../config.php";
 
 var_dump($_POST);
-echo $_SESSION['id'];
+echo $_SESSION["username"];
+
+$sql = "SELECT id_c FROM compte WHERE login ='" . $_SESSION["username"] ."';" ;
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$id_c = $stmt->fetch(PDO::FETCH_ASSOC)['id_c'];
+
+echo $id_c;
 
 $req = $pdo->prepare("INSERT INTO wishlist ( id_c , id_offre ) VALUES ( :id_c , :id_offre )");
-$req->bindParam(":id_c", $_SESSION['id']);
+$req->bindParam(":id_c", $id_c);
 $req->bindParam(":id_offre", $_POST['id_offre']);
 $req->execute();
+
+header("Location: listOffre.php");
 
 ?>
